ad08881108674ea1a158ccd8a4d64fdf93fda23f
Joseph <josephbryan.sena@gmail.com>
2020-05-13 19:34:10 +0800

    Microsoft Page -Finish mobile-first feature.  Editing burger menu contents.

---
+++ app.js
@@ -0,0 +1,8 @@
+function showNavmenu() {
+  var x = document.querySelector(".dropdown-content");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
