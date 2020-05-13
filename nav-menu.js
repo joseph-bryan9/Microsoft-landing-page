740cb4207ee9c888067a34595a51dcc710a52c97
Joseph <josephbryan.sena@gmail.com>
2020-05-14 01:17:03 +0800

    Microsoft Page - Editing burger menu contents v2.

--- nav-menu.js
+++ nav-menu.js
@@ -1,8 +1,132 @@
-function showNavmenu() {
-  var x = document.querySelector(".dropdown-content");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+function showSoftware() {
+  var dropdownContentDefaultHidden = document.getElementById("dropdown-1");
+  var dropdownContent = document.getElementById("dropdown-content-1");
+  //   var chevronDown = document.querySelector(".fa-chevron-down");
+  //   var chevronUp = document.querySelector(".fa-chevron-up");
+
+  if (
+    !dropdownContent.style.display ||
+    dropdownContent.style.display == "none"
+  ) {
+    dropdownContentDefaultHidden.style.display = "block";
+    dropdownContent.style.display = "block";
+    x.classList.toggle(".fa-chevron-up");
+    // chevronDown.style.display = "none";
+    // chevronUp.style.display = "block";
   } else {
-    x.style.display = "none";
+    dropdownContentDefaultHidden.style.display = "none";
+    dropdownContent.style.display = "none";
+    chevronUp.style.display = "none";
+    chevronDown.style.display = "block";
+  }
+}
+
+function showPCsDevices() {
+  var dropdownContentDefaultHidden = document.getElementById("dropdown-2");
+  var dropdownContent = document.getElementById("dropdown-content-2");
+  var chevronDown = document.querySelector(".fa-chevron-down");
+  var chevronUp = document.querySelector(".fa-chevron-up");
+
+  if (
+    !dropdownContent.style.display ||
+    dropdownContent.style.display == "none"
+  ) {
+    dropdownContentDefaultHidden.style.display = "block";
+    dropdownContent.style.display = "block";
+    chevronDown.style.display = "none";
+    chevronUp.style.display = "block";
+  } else {
+    dropdownContentDefaultHidden.style.display = "none";
+    dropdownContent.style.display = "none";
+    chevronUp.style.display = "none";
+    chevronDown.style.display = "block";
+  }
+}
+
+function showEntertainment() {
+  var dropdownContentDefaultHidden = document.getElementById("dropdown-3");
+  var dropdownContent = document.getElementById("dropdown-content-3");
+  var chevronDown = document.querySelector(".fa-chevron-down");
+  var chevronUp = document.querySelector(".fa-chevron-up");
+
+  if (
+    !dropdownContent.style.display ||
+    dropdownContent.style.display == "none"
+  ) {
+    dropdownContentDefaultHidden.style.display = "block";
+    dropdownContent.style.display = "block";
+    chevronDown.style.display = "none";
+    chevronUp.style.display = "block";
+  } else {
+    dropdownContentDefaultHidden.style.display = "none";
+    dropdownContent.style.display = "none";
+    chevronUp.style.display = "none";
+    chevronDown.style.display = "block";
+  }
+}
+
+function showBusiness() {
+  var dropdownContentDefaultHidden = document.getElementById("dropdown-4");
+  var dropdownContent = document.getElementById("dropdown-content-4");
+  var chevronDown = document.querySelector(".fa-chevron-down");
+  var chevronUp = document.querySelector(".fa-chevron-up");
+
+  if (
+    !dropdownContent.style.display ||
+    dropdownContent.style.display == "none"
+  ) {
+    dropdownContentDefaultHidden.style.display = "block";
+    dropdownContent.style.display = "block";
+    chevronDown.style.display = "none";
+    chevronUp.style.display = "block";
+  } else {
+    dropdownContentDefaultHidden.style.display = "none";
+    dropdownContent.style.display = "none";
+    chevronUp.style.display = "none";
+    chevronDown.style.display = "block";
+  }
+}
+
+function showDeveloperIT() {
+  var dropdownContentDefaultHidden = document.getElementById("dropdown-5");
+  var dropdownContent = document.getElementById("dropdown-content-5");
+  var chevronDown = document.querySelector(".fa-chevron-down");
+  var chevronUp = document.querySelector(".fa-chevron-up");
+
+  if (
+    !dropdownContent.style.display ||
+    dropdownContent.style.display == "none"
+  ) {
+    dropdownContentDefaultHidden.style.display = "block";
+    dropdownContent.style.display = "block";
+    chevronDown.style.display = "none";
+    chevronUp.style.display = "block";
+  } else {
+    dropdownContentDefaultHidden.style.display = "none";
+    dropdownContent.style.display = "none";
+    chevronUp.style.display = "none";
+    chevronDown.style.display = "block";
+  }
+}
+
+function showOther() {
+  var dropdownContentDefaultHidden = document.getElementById("dropdown-6");
+  var dropdownContent = document.getElementById("dropdown-content-6");
+  var chevronDown = document.querySelector(".fa-chevron-down");
+  var chevronUp = document.querySelector(".fa-chevron-up");
+
+  if (
+    !dropdownContent.style.display ||
+    dropdownContent.style.display == "none"
+  ) {
+    dropdownContentDefaultHidden.style.display = "block";
+    dropdownContent.style.display = "block";
+    chevronDown.style.display = "none";
+    chevronUp.style.display = "block";
+  } else {
+    dropdownContentDefaultHidden.style.display = "none";
+    dropdownContent.style.display = "none";
+    chevronUp.style.display = "none";
+    chevronDown.style.display = "block";
   }
 }
